3fa4724aaf37a85b96590ce716d8e6d92d802cad
Jigarkumar-Panchal <jigarkumar.panchal@st.ovgu.de>
2024-08-17 13:17:54 +0200

    Update Half_Adder.vhd

--- Basic_programming/Half_Adder_demonstration/Half_Adder.vhd
+++ Basic_programming/Half_Adder_demonstration/Half_Adder.vhd
@@ -8,7 +8,7 @@
 -- Project Name: Half_Adder
 -- Target Devices: BASYS 3
 -- Tool Versions: 
--- Description: Taking on board Switch 15 as input A,Switch 14 as input B. Taking on board LED 1 as input SUM, LED 0 as input CARRY.
+-- Description: Taking on board Switch 15 as input A,Switch 14 as input B. Taking on board LED 1 as output SUM, LED 0 as output CARRY.
 -- 
 -- Dependencies: 
 -- 
